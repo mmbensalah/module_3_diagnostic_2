9addec6e26bc6038bb58b97df12460217b534184
mmbensalah <mmbensalah@gmail.com>
2018-12-12 10:25:17 -0700

    with new service results, changed to reflect json response

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,18 @@
 class Station
-  def initialize(name, address, fuel_type, distance, access_days_time)
-    @name             = name
-    @address          = address
-    @fuel_type        = fuel_type
-    @distance         = distance
-    @access_days_time = access_days_time
+  attr_reader :name,
+              :street_address,
+              :state,
+              :zip,
+              :fuel_type,
+              :distance,
+              :access
+  def initialize(data)
+    @name             = data[:station_name]
+    @street_address   = data[:street_address]
+    @state            = data[:state]
+    @zip              = data[:zip]
+    @fuel_type        = data[:fuel_type_code]
+    @distance         = data[:distance]
+    @access           = data[:access_days_time]
   end
 end
