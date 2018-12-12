93bb55a6d947a49b2cbbd4571417680ad37bfde0
mmbensalah <mmbensalah@gmail.com>
2018-12-12 08:05:12 -0700

    Adds VCR configure block to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'webmock/rspec'
+require 'vcr'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+  config.filter_sensitive_data("<NREL_API_KEY>") { ENV['NREL_API_KEY'] }
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
