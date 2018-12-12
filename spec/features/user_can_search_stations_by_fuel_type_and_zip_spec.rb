28afccb895630336729a83b5236b4063f727f1ea
mmbensalah <mmbensalah@gmail.com>
2018-12-12 10:57:13 -0700

    finishes feature test

--- spec/features/user_can_search_stations_by_fuel_type_and_zip_spec.rb
+++ spec/features/user_can_search_stations_by_fuel_type_and_zip_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'User searches for' do
   it 'electric and propane stations within 6 miles' do
-    visit '/'
+    VCR.use_cassette('StationFacade') do
+      visit '/'
 
-    fill_in :q, with: '80203'
+      fill_in :q, with: '80203'
 
-    click_on 'Locate'
+      click_on 'Locate'
 
-    expect(current_path).to eq('/search')
-    binding.pry
+      expect(current_path).to eq('/search')
 
-    within .station do
-      expect(stations.count).to eq(10)
+      within('.station') do
+        expect(page).to have_content("24 hours daily")
+        expect(page).to have_content("0.31422")
+        expect(page).to have_content("ELEC")
+        expect(page).to have_content("UDR")
+        expect(page).to have_content("CO")
+        expect(page).to have_content("800 Acoma St")
+        expect(page).to have_content("80203")
+      end
     end
   end
 end
